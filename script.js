06d43d0875aa447f46c12468dbf969fdb2e92dbf
Ianca <Bdias3155@gmail.com>
2024-11-09 18:21:02 -0300

    alteracaoJs

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function toggleMode(){
-  const html = document.documentElement
-  html.classList.toggle('light')
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('light');
 
-  //pegar a tag img
-  const img = document.querySelector("profile img")
+  // pegar a tag img
+  const img = document.querySelector("#profile img"); // Adjusted selector
 
-  //substituir a imagem
-   if(html.classList.contains('light')) {
-    //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
-    } else {
-    //se tiver light mode,manter a imagem normal
-    img.setAttribute("src", "./assets/avatar-ianca.jpeg")
-    }
+  // substituir a imagem
+  if (html.classList.contains('light')) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/animacionOff.png");
+  } else {
+    // se não tiver light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/animacion.png");
+  }
 }
